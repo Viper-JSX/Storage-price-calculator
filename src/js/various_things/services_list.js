dae58df0cc915111dcc7a1db4ab503fffa7fa62e
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-01-29 12:27:50 +0200

    Declared variable servicesList that holds all services data and their data

---
+++ src/js/various_things/services_list.js
@@ -0,0 +1,71 @@
+export const servicesList = [
+    {
+        name: "backbaze.com",
+        pricing: {
+            storage: 0.005,
+            transfer: 0.001
+        },
+        limits: {
+            minPay: 7,
+            maxPay: Infinity
+        },
+        hddOrSsdOption: false,
+        singleOMultiOption: false,
+        discounts: null
+    }, 
+
+    {
+        name: "bunny.net",
+        pricing: {
+            storage: {
+                hdd: 0.01,
+                ssd: 0.02
+            },
+            transfer: 0.01
+        },
+        limits: {
+            minPay: 0,
+            maxPay: 10
+        },
+        hddOrSsdOption: true,
+        singleOMultiOption: false,
+        discounts: null
+    }, 
+
+    {
+        name: "scaleway.com",
+        pricing: {
+            storage: {
+                single: 0.03,
+                multi: 0.06
+            },
+            transfer: 0.02
+        },
+        limits: null,
+        hddOrSsdOption: false,
+        singleOMultiOption: true,
+        discounts: {
+            storage: {
+                gigabytesForFree: 75
+            },
+            transfer: {
+                gigabytesForFree: 75
+            }
+        }
+    }, 
+
+    {
+        name: "vultrl.com",
+        pricing: {
+            storage: 0.01,
+            transfer: 0.01
+        },
+        limits: {
+            minPay: 5,
+            maxPay: Infinity
+        },
+        hddOrSsdOption: false,
+        singleOMultiOption: false,
+        discounts: null
+    },   
+];
