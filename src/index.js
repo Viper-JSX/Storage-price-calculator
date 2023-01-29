410f790dd4e689dd34b32693ddaaa6809abb1440
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-01-29 12:34:13 +0200

    Initialized the state of services, this state is used to track currently selected storage type and other options that can be changed by the user

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import "./css/main.css";
+import { servicesList } from "./js/various_things/services_list";
+import { HDD, SINGLE } from "./js/various_things/word_constants";
 
 let storageSize = 0;
 let transferSize = 0;
+
+const servicesState = servicesList.map((service) => {
+    const serviceState = {
+        service: JSON.parse(JSON.stringify(service)) //To remove all object references
+    };
+
+    if(service.pricing.storage.hdd){ // HDD or SSD
+        serviceState.storageType = HDD; 
+    }
+    else if(service.pricing.storage.multi){ // single or multi
+        serviceState.storageType = SINGLE;
+    }
+
+    return serviceState;
+});
+
+console.log(servicesState);
